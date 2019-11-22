c2cdebb465e42d56bef8e5c09b40f8f5ea04e621
Rizzo <dominic.rizzo@ou.edu>
2019-11-22 12:54:11 -0600

    Added basic curl functionality

--- app/src/main/java/com/example/rizzo_seandroidmidterm/MainActivity.kt
+++ app/src/main/java/com/example/rizzo_seandroidmidterm/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.rizzo_seandroidmidterm
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,10 +10,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-
-
-
+        button.setOnClickListener {
+            val process = ProcessBuilder("curl", "https://cs-4263-252417.appspot.com").start()
+            process.inputStream.reader(Charsets.UTF_8).use {
+                println(it.readText())
+            }
+        }
 
     }
 }
