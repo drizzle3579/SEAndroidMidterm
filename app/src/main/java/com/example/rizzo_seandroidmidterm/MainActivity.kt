3f53052b3b3067e0b53929e18602bf8f528d1769
Rizzo <dominic.rizzo@ou.edu>
2019-11-22 15:45:29 -0600

    Had to alter methods and dependencies used, added basic threading

--- app/src/main/java/com/example/rizzo_seandroidmidterm/MainActivity.kt
+++ app/src/main/java/com/example/rizzo_seandroidmidterm/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.rizzo_seandroidmidterm
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,12 +13,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        button.setOnClickListener {
-            val process = ProcessBuilder("curl", "https://cs-4263-252417.appspot.com").start()
-            process.inputStream.reader(Charsets.UTF_8).use {
-                println(it.readText())
-            }
+        fun query() {
+            val result = URL("https://cs-4263-252417.appspot.com").readText()
+            println(result)
         }
 
+        button.setOnClickListener {
+            GlobalScope.launch {
+                query()
+            }
+        }
     }
 }
